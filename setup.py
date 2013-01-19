bf35fec46c91d52a78c2a7492ace8d84c907d439
Dijam <dijam@Dijam-Pro.local>
2013-01-19 11:26:17 +0100

    Setup and webservice

---
+++ setup.py
@@ -0,0 +1,13 @@
+setup(
+    name = "mhd5",
+    version = "0.0.1",
+    author = "Mhd5",
+    author_email = "majid.sadeghi@gmail.com",
+    description = ("Music Hack Day 2013"),
+    url = "http://packages.python.org/an_example_pypi_project",
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Topic :: Music"
+    ],
+)
