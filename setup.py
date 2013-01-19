cefce4da33eb30e4dc34b22a738ad2755c25f746
Dijam <dijam@Dijam-Pro.local>
2013-01-19 11:49:46 +0100

    Correcting setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
+from setuptools import setup, find_packages
+
 setup(
-    name = "mhd5",
-    version = "0.0.1",
-    author = "Mhd5",
-    author_email = "majid.sadeghi@gmail.com",
-    description = ("Music Hack Day 2013"),
-    url = "http://packages.python.org/an_example_pypi_project",
-    long_description=read('README'),
-    classifiers=[
-        "Development Status :: 1 - Alpha",
-        "Topic :: Music"
-    ],
+    name = "MHD5",
+    version = "0.1",
+    packages = find_packages(),
 )
